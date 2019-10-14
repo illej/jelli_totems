8d19768db24159a695514a016d61dd861bba6162
Elliot Ayrey <eha2@arastudent.ac.nz>
2019-10-14 19:53:25 +1300

    securehandlers working

--- jelli_totems_main.lua
+++ jelli_totems_main.lua
@@ -147,6 +147,186 @@ function bind_elements(totems)
     print(' ')
 end
 
+
+-- And so begins the secure death loop
+
+-- CreateFrame("Button", "ElementBinder", UIParent, "SecureHandlerClickTemplate")
+-- TotemPanel:SetAttribute("_onclick", [[
+--   self:GetChildren():SetAttribute("_type", rebind_phase_1)
+-- ]])
+
+
+-- CreateFrame("Button", "RotateSpellCast", RotateSpellNext, "ecureHandlerClickTemplate")
+-- RotateSpellCast:SetAttribute("type", "spell")
+-- RotateSpellCast:SetAttribute("spell", "Flame Shock")
+
+-- -- temp
+
+-- local element_binder = CreateFrame("BUTTON", "ElementBinder", UIParent, "SecureHandlerClickTemplate");
+-- element_binder:SetAttribute("_onclick", [=[
+--     if button == "LeftButton" then    
+--         self:SetBindingClick(true, "1", "EarthBtn")
+--         self:SetBindingClick(true, "2", "FireBtn")
+--         -- etc for water and air
+--     end
+-- ]=]);
+
+-- local earth_btn = CreateFrame("BUTTON", "EarthBtn", element_binder, "SecureHandlerClickTemplate")
+-- earth_btn:SetAttribute("_onclick", [=[
+--     if button == "LeftButton" then
+--         self:SetBindingSpell(true, "1", "Earthbind Totem")
+--         self:SetBindingSpell(true, "2", "Strength of Earth Totem")
+--         -- etc for the rest of the earth totems
+--     end
+-- ]=])
+
+-- local fire = CreateFrame("BUTTON", "fire", panel, "SecureHandlerClickTemplate")
+-- fire:SetAttribute("_onclick", [=[
+--     if button == "LeftButton" then
+--         print('> fire')
+--         self:SetBindingSpell(true, "1", "Searing Totem")
+--         self:SetBindingSpell(true, "2", "Fire Nova Totem")
+--     end
+-- ]=])
+-- fire:WrapScript(fire, "PostClick", [[
+--     print('post')
+--     local p = self:GetParent()
+--     p:ClearBindings()    
+-- ]])
+
+-- dude on forums
+
+-- The button configs
+local Config = {
+    {
+        {
+            type = "macro",
+            macrotext = "/run print('1-1')"
+        },
+        {
+            type = "macro",
+            macrotext = "/run print('1-2')"
+        },
+    },
+    {
+        {
+            type = "macro",
+            macrotext = "/run print('2-1')"
+        },
+    },
+    {
+        {
+            type = "macro",
+            macrotext = "/run print('3-1')"
+        },
+    },
+}
+ 
+-- A mananger frame to control all secure behaviors
+local _ManagerFrame = CreateFrame("Frame", "TwoLevelKeyManager", UIParent, "SecureHandlerStateTemplate")
+ 
+_ManagerFrame.Execute = SecureHandlerExecute
+_ManagerFrame.WrapScript = function(self, frame, script, preBody, postBody) return SecureHandlerWrapScript(frame, script, self, preBody, postBody) end
+_ManagerFrame.SetFrameRef = SecureHandlerSetFrameRef
+ 
+_ManagerFrame:Execute[[
+    Manager = self
+ 
+    ToggleButtons = newtable()
+    ToggleState = newtable()
+ 
+    -- secure code snippets to be run by manager
+    CloseFinalLevel = [==[
+        local index = ...
+        print("CloseFinalLevel", index)
+        if ToggleState[index] then
+            ToggleState[index] = false
+ 
+            for i, btn in ipairs(ToggleButtons[index]) do
+                print("Clear Bind", btn:GetName())
+                btn:ClearBinding("" .. i)
+            end
+        end
+        print("Clear Bind", ToggleButtons[index][0]:GetName())
+        ToggleButtons[index][0]:ClearBinding("" .. index)
+    ]==]
+ 
+    OpenFinalLevel = [==[
+        local index = ...
+        print("OpenFinalLevel", index)
+        if not ToggleState[index] then
+            ToggleState[index] = true
+ 
+            for i, btn in ipairs(ToggleButtons[index]) do
+                print("Bind", btn:GetName())
+                btn:SetBindingClick(true, "" .. i, btn:GetName(), "LeftButton")
+            end
+        end
+    ]==]
+ 
+    ToggleRoot = [==[
+        print("ToggleRoot", not ToggleState[0])
+        if ToggleState[0] then
+            -- Clear binding for all
+            ToggleState[0] = false
+ 
+            for i, buttons in ipairs(ToggleButtons) do
+                Manager:Run(CloseFinalLevel, i)
+            end
+        else
+            -- Bind key to first level
+            ToggleState[0] = true
+ 
+            for i, buttons in ipairs(ToggleButtons) do
+                print("Bind", buttons[0]:GetName())
+                buttons[0]:SetBindingClick(true, "" .. i, buttons[0]:GetName(), "LeftButton")
+            end
+        end
+    ]==]
+]]
+ 
+-- Create buttons
+local rootbtn = CreateFrame("CheckButton", "RootToggle", UIParent, "SecureActionButtonTemplate")
+ 
+-- Bind the root key
+SetOverrideBindingClick(rootbtn, true, "F", "RootToggle", "LeftButton")
+ 
+-- Toggle the first level button's key binding
+_ManagerFrame:WrapScript(rootbtn, "OnClick", [[Manager:Run(ToggleRoot)]])
+ 
+-- Generate the first and second level buttons and register them to the mananger
+for i, firstlvl in ipairs(Config) do
+    local btn = CreateFrame("CheckButton", "FirstLvlToggle" .. i, UIParent, "SecureActionButtonTemplate")
+    _ManagerFrame:SetFrameRef("FirstLvlToggle", btn)
+ 
+    for j, nxtlvl in ipairs(firstlvl) do
+        local fbtn = CreateFrame("CheckButton", "NxtLvlToggle" .. i .. "_" .. j, UIParent, "SecureActionButtonTemplate")
+        _ManagerFrame:SetFrameRef("NxtLvlToggle" .. j, fbtn)
+ 
+        for k, v in pairs(nxtlvl) do
+            fbtn:SetAttribute(k, v) -- Bind marco
+        end
+ 
+        -- Clear all key bindings after click the second level button
+        _ManagerFrame:WrapScript(fbtn, "OnClick", [[return button, true]], [[Manager:Run(ToggleRoot)]])
+    end
+ 
+    _ManagerFrame:Execute(string.format([[
+        local index, count = %d, %d
+        local buttons = newtable()
+        buttons[0] = Manager:GetFrameRef("FirstLvlToggle")
+        print("Register: ", index, buttons[0]:GetName())
+        for i = 1, count do
+            buttons[i] = Manager:GetFrameRef("NxtLvlToggle" .. i)
+            print("Register: ", buttons[i]:GetName())
+        end
+ 
+        ToggleButtons[index] = buttons
+    ]], i, #firstlvl))
+ 
+    -- Bind the key to the second level
+    _ManagerFrame:WrapScript(btn, "OnClick", string.format([[Manager:Run(OpenFinalLevel, %d)]], i))
+end
 local panel = CreateFrame("BUTTON", "TotemPanel", UIParent, "SecureHandlerClickTemplate");
 panel:SetAttribute("_onclick", [=[
     if button == "LeftButton" then    
